ecbea39bf83768bcd42aaf47bef71b569d975836
Jason Bullard <jason@bullard.io>
2024-08-02 14:18:34 -0500

    last min fixes

--- ui/src/LaunchBuilder.jsx
+++ ui/src/LaunchBuilder.jsx
@@ -1,176 +1,186 @@
-import React, { useState, useEffect } from 'react';
-import { Card } from 'primereact/card';
-import { Dropdown } from 'primereact/dropdown';
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
+import React, { useState, useEffect } from 'react'
+import { Card } from 'primereact/card'
+import { Dropdown } from 'primereact/dropdown'
+import { Button } from 'primereact/button'
+import { DataTable } from 'primereact/datatable'
+import { Column } from 'primereact/column'
 
 export const LaunchBuilder = () => {
-  const [launches, setLaunches] = useState([]);
-  const [selectedLaunch, setSelectedLaunch] = useState(null);
+  const [launches, setLaunches] = useState([])
+  const [selectedLaunch, setSelectedLaunch] = useState(null)
 
-  const [rooms, setRooms] = useState([]);
+  const [rooms, setRooms] = useState([])
   const [selectedRoom, setSelectedRoom] = useState(null)
 
-  const [stations, setStations] = useState([]);
-  const [selectedStation, setSelectedStation] = useState(null);
+  const [stations, setStations] = useState([])
+  const [selectedStation, setSelectedStation] = useState(null)
 
   const [orgs, setOrgs] = useState([])
   const [selectedOrg, setSelectedOrg] = useState(null)
-  
+
   const [names, setNames] = useState([
     { id: 1, name: 'Mr. Somebody', org: 'Boeing', dutyTitle: 'SPO' },
     { id: 2, name: 'Mr. Somebodyelse', org: 'Boeing', dutyTitle: 'SPO' },
     { id: 3, name: 'Mr. anotherperson', org: 'Boeing', dutyTitle: 'SPO' },
-  ]);
+  ])
 
   useEffect(() => {
     const fetchLaunches = async () => {
       try {
-        const response = await fetch('http://localhost:3002/api/v1/builds/');
+        const response = await fetch('http://localhost:3002/api/v1/builds/')
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok')
         }
-        const launches = await response.json();
+        const launches = await response.json()
         setLaunches(launches)
       } catch (error) {
-        console.error('Error fetching launches:', error);
+        console.error('Error fetching launches:', error)
       }
-    };
+    }
 
-    fetchLaunches();
-  }, []);
-
-
+    fetchLaunches()
+  }, [])
 
   const addRoom = async () => {
     if (selectedLaunch) {
       try {
-        const response = await fetch(`http://localhost:3002/api/v1/builds/${selectedLaunch}/rooms`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
+        const response = await fetch(
+          `http://localhost:3002/api/v1/builds/${selectedLaunch}/rooms`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name: `Room ${rooms.length + 1}` }),
           },
-          body: JSON.stringify({ name: `Room ${rooms.length + 1}` }),
-        });
+        )
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok')
         }
-        const newRoom = await response.json();
-        setRooms([...rooms, { id: newRoom.id, launch_id: selectedLaunch, room_id: newRoom.room_id }]);
+        const newRoom = await response.json()
+        setRooms([
+          ...rooms,
+          {
+            id: newRoom.id,
+            launch_id: selectedLaunch,
+            room_id: newRoom.room_id,
+          },
+        ])
       } catch (error) {
-        console.error('Error adding room:', error);
+        console.error('Error adding room:', error)
       }
     }
-  };
+  }
 
   useEffect(() => {
     const fetchRooms = async () => {
       if (selectedLaunch) {
         try {
-          const response = await fetch(`http://localhost:3002/api/v1/builds/${selectedLaunch}/rooms?extended=true`);
+          const response = await fetch(
+            `http://localhost:3002/api/v1/builds/${selectedLaunch}/rooms?extended=true`,
+          )
           if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok')
           }
-          const rooms = await response.json();
+          const rooms = await response.json()
           setRooms(rooms)
-
         } catch (error) {
-          console.error('Error fetching rooms:', error);
+          console.error('Error fetching rooms:', error)
         }
       }
-    };
+    }
 
-    fetchRooms();
-  }, [selectedLaunch]);
+    fetchRooms()
+  }, [selectedLaunch])
 
   useEffect(() => {
     const fetchRoomSeats = async () => {
       if (selectedRoom) {
         try {
-          const response = await fetch(`http://localhost:3002/api/v1/rooms/${selectedRoom}/stations`);
+          const response = await fetch(
+            `http://localhost:3002/api/v1/rooms/${selectedRoom}/stations`,
+          )
           if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok')
           }
-          const {stations} = await response.json();
-          setStations(stations);
+          const { stations } = await response.json()
+          setStations(stations)
         } catch (error) {
-          console.error('Error fetching seats:', error
-          );
+          console.error('Error fetching seats:', error)
+        }
       }
     }
-  };
-  
-  fetchRoomSeats();
-  
+
+    fetchRoomSeats()
   }, [selectedRoom])
 
-useEffect(() => {
-  const fetchOrgs = async () => {
-    if (selectedOrg) {
-      try{
-        const response = await fetch('http://localhost:3002/api/v1/orgs/');
+  useEffect(() => {
+    const fetchOrgs = async () => {
+      try {
+        const response = await fetch('http://localhost:3002/api/v1/orgs/')
         if (!response.ok) {
-          throw new Error ('Network reponse was not ok');
+          throw new Error('Network reponse was not ok')
         }
-        const {orgs} = await response.json();
+        const orgs = await response.json()
+        console.log(orgs)
         setOrgs(orgs)
       } catch (error) {
-        console.error('Error fetching launches:', error);
+        console.error('Error fetching launches:', error)
       }
     }
-  }
-    
-    fetchOrgs();
-  }, [selectedOrg]);
 
+    fetchOrgs()
+  }, [])
 
   return (
     <div>
       <Card>
-        <h1 className="text-2xl font-bold mb-4">Launch builder page - Admin</h1>
+        <h1 className='text-2xl font-bold mb-4'>Launch builder page - Admin</h1>
 
-        <div className="grid grid-cols-2 gap-4">
+        <div className='grid grid-cols-2 gap-4'>
           <div>
-            <h2 className="text-xl mb-2">launch builder</h2>
+            <h2 className='text-xl mb-2'>launch builder</h2>
             <Dropdown
-              className="w-full mb-2"
-              placeholder="select launch"
+              className='w-full mb-2'
+              placeholder='select launch'
               options={launches}
-              optionLabel="name"
-              optionValue="id"
+              optionLabel='name'
+              optionValue='id'
               value={selectedLaunch}
               onChange={(e) => setSelectedLaunch(e.value)}
             />
 
             {selectedLaunch && (
               <>
-              <h3>Rooms</h3>
-              <Dropdown
-                placeholder="select room"
-                options={rooms}
-                optionLabel="name"
-                optionValue="id"
-                value={selectedRoom}
-                onChange={(e) => setSelectedRoom(e.value)}
-              />
+                <h3>Rooms</h3>
+                <Dropdown
+                  placeholder='select room'
+                  options={rooms}
+                  optionLabel='name'
+                  optionValue='id'
+                  value={selectedRoom}
+                  onChange={(e) => setSelectedRoom(e.value)}
+                />
 
-              <Button label="Add Room" onClick={addRoom} className="p-button-outlined w-full" />
+                <Button
+                  label='Add Room'
+                  onClick={addRoom}
+                  className='p-button-outlined w-full'
+                />
               </>
             )}
 
             {selectedRoom && (
               <>
                 <h3>Stations</h3>
-              <Dropdown
-                placeholder="select station"
-                options={stations}
-                optionLabel="name"
-                optionValue="id"
-                value={selectedStation}
-                onChange={(e) => setSelectedStation(e.value)}
-              />
+                <Dropdown
+                  placeholder='select station'
+                  options={stations}
+                  optionLabel='name'
+                  optionValue='id'
+                  value={selectedStation}
+                  onChange={(e) => setSelectedStation(e.value)}
+                />
               </>
             )}
 
@@ -178,30 +188,33 @@ useEffect(() => {
           </div>
 
           <div>
-            <h2 className="text-xl mb-2">Names</h2>
+            <h2 className='text-xl mb-2'>Names</h2>
             <Dropdown
-              placeholder="ORG" 
-              optons={orgs}
-              optionLabel="name"
-              optionvalue="id"
+              placeholder='select org'
+              options={orgs}
+              optionLabel='name'
+              optionValue='id'
               value={selectedOrg}
               onChange={(e) => setSelectedOrg(e.value)}
             />
-            
-            <Dropdown filter className="w-full mb-2" placeholder="Duty Title" />
 
-            <DataTable value={names} className="mt-2" datakey='id'>
-              <Column field="name" header="Name" />
-              <Column field="org" header="Org" />
-              <Column field="dutyTitle" header="Duty Title" />
+            <Dropdown filter className='w-full mb-2' placeholder='Duty Title' />
+
+            <DataTable value={names} className='mt-2' datakey='id'>
+              <Column field='name' header='Name' />
+              <Column field='org' header='Org' />
+              <Column field='dutyTitle' header='Duty Title' />
             </DataTable>
 
-            <Button label="assign attendee" className="p-button-outlined mt-4" />
+            <Button
+              label='assign attendee'
+              className='p-button-outlined mt-4'
+            />
           </div>
         </div>
       </Card>
     </div>
-  );
-};
+  )
+}
 
-export default LaunchBuilder;
+export default LaunchBuilder
--- ui/src/OrgManager.jsx
+++ ui/src/OrgManager.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { Card } from 'primereact/card';
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { Panel } from 'primereact/panel';
-import { PickList } from 'primereact/picklist';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Card } from 'primereact/card'
+import { InputText } from 'primereact/inputtext'
+import { Button } from 'primereact/button'
+import { DataTable } from 'primereact/datatable'
+import { Column } from 'primereact/column'
+import { Panel } from 'primereact/panel'
+import { PickList } from 'primereact/picklist'
+import { Link } from 'react-router-dom'
 
 export const OrgManager = () => {
-  const [organization, setOrganization] = useState(null);
-  const { orgId, maxAttendees } = useParams();
+  const [organization, setOrganization] = useState(null)
+  const { id: orgId, maxAttendees } = useParams()
   const [source, setSource] = useState([
     { name: 'Mr. Somebody', org: '5 SLS', dutyTitle: 'FML' },
     { name: 'Mr. Somebodyelse', org: 'SPO', dutyTitle: 'Prop' },
     { name: 'Mr. anotherperson', org: 'SSC', dutyTitle: 'Eng' },
-  ]);
+  ])
   const [roster, setRoster] = useState([])
 
   const [newAttendee, setNewAttendee] = useState({
@@ -24,117 +24,171 @@ export const OrgManager = () => {
     org: '',
     dutyTitle: '',
     phone: '',
-    email: ''
-  });
+    email: '',
+  })
 
   const [target, setTarget] = useState([
-    { name: 'Mr. anotherperson', org: 'Boeing', dutyTitle: 'SPO', phone: 'XXX-XXX-XXXX', email: '' },
-    { name: 'Mr. Somebody', org: 'Boeing', dutyTitle: 'SPO', phone: 'XXX-XXX-XXXX', email: '' },
-  ]);
+    {
+      name: 'Mr. anotherperson',
+      org: 'Boeing',
+      dutyTitle: 'SPO',
+      phone: 'XXX-XXX-XXXX',
+      email: '',
+    },
+    {
+      name: 'Mr. Somebody',
+      org: 'Boeing',
+      dutyTitle: 'SPO',
+      phone: 'XXX-XXX-XXXX',
+      email: '',
+    },
+  ])
 
   useEffect(() => {
     // Fetch organization data
     const fetchOrganization = async () => {
       try {
-        const response = await fetch(`http://localhost:3002/api/v1/orgs/${orgId}`);
-        const data = await response.json();
-        setOrganization(data);
+        const response = await fetch(
+          `http://localhost:3002/api/v1/orgs/${orgId}`,
+        )
+        const data = await response.json()
+        setOrganization(data)
       } catch (error) {
-        console.error('Error fetching organization:', error);
+        console.error('Error fetching organization:', error)
       }
-    };
+    }
 
     // Fetch attendees data from directory
     const fetchAttendees = async () => {
       try {
-        const response = await fetch('http://localhost:3002/api/v1/directory');
-        const data = await response.json();
+        const response = await fetch('http://localhost:3002/api/v1/directory')
+        const data = await response.json()
 
         // Filter attendees by orgId and set the source
-        const filteredAttendees = data.filter(attendee => attendee.org_id === orgId)
-          .map(attendee => ({
+        const filteredAttendees = data
+          .filter((attendee) => attendee.org_id === orgId)
+          .map((attendee) => ({
             name: attendee.name,
             org: attendee.org,
             dutyTitle: attendee.dutyTitle,
             phone: attendee.phone || '',
-            email: attendee.email || ''
-          }));
-
-
+            email: attendee.email || '',
+          }))
       } catch (error) {
-        console.error('Error fetching attendees:', error);
+        console.error('Error fetching attendees:', error)
       }
-    };
+    }
 
-    fetchOrganization();
-    fetchAttendees();
-  }, [orgId]);
+    fetchOrganization()
+    fetchAttendees()
+  }, [orgId])
 
   const onChange = (event) => {
-    setSource(event.source);
-    setTarget(event.target);
-  };
+    setSource(event.source)
+    setTarget(event.target)
+  }
 
   const itemTemplate = (item) => {
     return (
-      <div className="flex flex-col p-2">
-
-        <span className="font-bold mb-2">{item.name}</span>
-        <span>{item.org} - {item.dutyTitle}</span>
+      <div className='flex flex-col p-2'>
+        <span className='font-bold mb-2'>{item.name}</span>
+        <span>
+          {item.org} - {item.dutyTitle}
+        </span>
       </div>
-    );
-  };
+    )
+  }
   const renderCreateAttendeePanel = () => (
-    <Panel className="mb-2">
-      <div className="p-fluid">
-        <div className="p-field">
-          <InputText placeholder="Name" value={newAttendee.name} onChange={(e) => createAttendee('name', e.target.value)} />
+    <Panel className='mb-2'>
+      <div className='p-fluid'>
+        <div className='p-field'>
+          <InputText
+            placeholder='Name'
+            value={newAttendee.name}
+            onChange={(e) => createAttendee('name', e.target.value)}
+          />
         </div>
-        <div className="p-field">
-          <InputText placeholder="Org" value={newAttendee.org} onChange={(e) => createAttendee('org', e.target.value)} />
+        <div className='p-field'>
+          <InputText
+            placeholder='Org'
+            value={newAttendee.org}
+            onChange={(e) => createAttendee('org', e.target.value)}
+          />
         </div>
-        <div className="p-field">
-          <InputText placeholder="Duty title" value={newAttendee.dutyTitle} onChange={(e) => createAttendee('dutyTitle', e.target.value)} />
+        <div className='p-field'>
+          <InputText
+            placeholder='Duty title'
+            value={newAttendee.dutyTitle}
+            onChange={(e) => createAttendee('dutyTitle', e.target.value)}
+          />
         </div>
-        <div className="p-field">
-          <InputText placeholder="Phone" value={newAttendee.phone} onChange={(e) => createAttendee('phone', e.target.value)} />
+        <div className='p-field'>
+          <InputText
+            placeholder='Phone'
+            value={newAttendee.phone}
+            onChange={(e) => createAttendee('phone', e.target.value)}
+          />
         </div>
-        <div className="p-field">
-          <InputText placeholder="Email" value={newAttendee.email} onChange={(e) => createAttendee('email', e.target.value)} />
+        <div className='p-field'>
+          <InputText
+            placeholder='Email'
+            value={newAttendee.email}
+            onChange={(e) => createAttendee('email', e.target.value)}
+          />
         </div>
       </div>
-      <Button label="Add Attendee" onClick={addNewAttendee} className="mt-2" />
+      <Button label='Add Attendee' onClick={addNewAttendee} className='mt-2' />
     </Panel>
-  );
+  )
 
   const renderAttendeePanel = (attendee, index) => (
-    <Panel key={index} className="mb-2">
-      <div className="p-fluid">
-        <div className="p-field">
-          <InputText placeholder="Name" value={attendee.name} onChange={(e) => updateAttendee(index, 'name', e.target.value)} />
+    <Panel key={index} className='mb-2'>
+      <div className='p-fluid'>
+        <div className='p-field'>
+          <InputText
+            placeholder='Name'
+            value={attendee.name}
+            onChange={(e) => updateAttendee(index, 'name', e.target.value)}
+          />
         </div>
-        <div className="p-field">
-          <InputText placeholder="Org" value={attendee.org} onChange={(e) => updateAttendee(index, 'org', e.target.value)} />
+        <div className='p-field'>
+          <InputText
+            placeholder='Org'
+            value={attendee.org}
+            onChange={(e) => updateAttendee(index, 'org', e.target.value)}
+          />
         </div>
-        <div className="p-field">
-          <InputText placeholder="Duty title" value={attendee.dutyTitle} onChange={(e) => updateAttendee(index, 'dutyTitle', e.target.value)} />
+        <div className='p-field'>
+          <InputText
+            placeholder='Duty title'
+            value={attendee.dutyTitle}
+            onChange={(e) => updateAttendee(index, 'dutyTitle', e.target.value)}
+          />
         </div>
-        <div className="p-field">
-          <InputText placeholder="Phone" value={attendee.phone} onChange={(e) => updateAttendee(index, 'phone', e.target.value)} />
+        <div className='p-field'>
+          <InputText
+            placeholder='Phone'
+            value={attendee.phone}
+            onChange={(e) => updateAttendee(index, 'phone', e.target.value)}
+          />
         </div>
-        <div className="p-field">
-          <InputText placeholder="Email" value={attendee.email} onChange={(e) => updateAttendee(index, 'email', e.target.value)} />
+        <div className='p-field'>
+          <InputText
+            placeholder='Email'
+            value={attendee.email}
+            onChange={(e) => updateAttendee(index, 'email', e.target.value)}
+          />
         </div>
       </div>
     </Panel>
-  );
+  )
 
   const createAttendee = (field, value) => {
-    setNewAttendee(prev => ({
+    setNewAttendee((prev) => ({
       ...prev,
-      [field]: value
-    }));
-  };
+      [field]: value,
+    }))
+  }
 
   const addNewAttendee = async () => {
     if (newAttendee.name && newAttendee.org && newAttendee.dutyTitle) {
@@ -145,24 +199,24 @@ export const OrgManager = () => {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify(newAttendee),
-        });
-        const addedAttendee = await response.json();
-        setSource(prev => [...prev, addedAttendee]);
+        })
+        const addedAttendee = await response.json()
+        setSource((prev) => [...prev, addedAttendee])
         setNewAttendee({
           name: '',
           org: '',
           dutyTitle: '',
           phone: '',
-          email: ''
-        });
+          email: '',
+        })
       } catch (error) {
-        console.error('Error adding new attendee:', error);
+        console.error('Error adding new attendee:', error)
       }
     }
   }
 
   const updateAttendee = async (index, field, value) => {
-    const updatedAttendee = { ...target[index], [field]: value };
+    const updatedAttendee = { ...target[index], [field]: value }
     try {
       const response = await fetch(`/api/v1/directory/${updatedAttendee.id}`, {
         method: 'PUT',
@@ -170,43 +224,51 @@ export const OrgManager = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(updatedAttendee),
-      });
-      const updatedAttendeeFromServer = await response.json();
-      const updatedTarget = [...target];
-      updatedTarget[index] = updatedAttendeeFromServer;
-      setTarget(updatedTarget);
+      })
+      const updatedAttendeeFromServer = await response.json()
+      const updatedTarget = [...target]
+      updatedTarget[index] = updatedAttendeeFromServer
+      setTarget(updatedTarget)
     } catch (error) {
-      console.error('Error updating attendee:', error);
+      console.error('Error updating attendee:', error)
     }
-  };
+  }
 
   return (
     <div>
-      <Link to='/'><Button>HOME</Button></Link>
-      <Card title={`Org manager page - ${organization ? organization.name : 'Loading...'}`}>
-        <div className="grid">
-          <div className="col-12 md:col-6">
-            <Card title="Attendee Selection" className="h-full">
+      <Link to='/'>
+        <Button>HOME</Button>
+      </Link>
+      <Card
+        title={`Org manager page - ${
+          organization ? organization.name : 'Loading...'
+        }`}
+      >
+        <div className='grid'>
+          <div className='col-12 md:col-6'>
+            <Card title='Attendee Selection' className='h-full'>
               <PickList
                 source={source}
                 target={target}
                 onChange={onChange}
                 itemTemplate={itemTemplate}
                 filter
-                filterBy="full_name,duty_title"
-                breakpoint="1280px"
-                sourceHeader="Available"
-                targetHeader="Selected"
+                filterBy='full_name,duty_title'
+                breakpoint='1280px'
+                sourceHeader='Available'
+                targetHeader='Selected'
                 sourceStyle={{ height: '24rem' }}
                 targetStyle={{ height: '24rem' }}
-                sourceFilterPlaceholder="Search by name or duty title"
-                targetFilterPlaceholder="Search by name or duty title"
+                sourceFilterPlaceholder='Search by name or duty title'
+                targetFilterPlaceholder='Search by name or duty title'
               />
-              <div className="text-orange-500 mt-4">Max Attendees: {maxAttendees}</div>
+              <div className='text-orange-500 mt-4'>
+                Max Attendees: {maxAttendees}
+              </div>
             </Card>
           </div>
-          <div className="col-12 md:col-6">
-            <Card title="Selected Attendees" className="h-full">
+          <div className='col-12 md:col-6'>
+            <Card title='Selected Attendees' className='h-full'>
               {target.map(renderAttendeePanel)}
             </Card>
             <Card>
@@ -217,5 +279,5 @@ export const OrgManager = () => {
         </div>
       </Card>
     </div>
-  );
-};
+  )
+}
